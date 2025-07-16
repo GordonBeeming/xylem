d0439acdfeb9879f2cb857b2e113c32779a07878
Gordon Beeming <me@gordonbeeming.com>
2025-07-17 00:18:24 +1000

    feat: enable Google Analytics with tracking ID
    Uncomment the Google Analytics configuration and set the
    googleAnalyticsId to 'G-W0FD111Z7V' in siteMetadata.js to enable
    analytics tracking. This change allows the site to collect usage
    data for better insights and performance monitoring.

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -45,9 +45,9 @@ const siteMetadata = {
     // posthogAnalytics: {
     //   posthogProjectApiKey: '', // e.g. 123e4567-e89b-12d3-a456-426614174000
     // },
-    // googleAnalytics: {
-    //   googleAnalyticsId: '', // e.g. G-XXXXXXX
-    // },
+    googleAnalytics: {
+      googleAnalyticsId: 'G-W0FD111Z7V',
+    },
   },
   comments: {
     // If you want to use an analytics provider you have to add it to the

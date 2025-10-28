2e3f922f9c51d5c10ec73e43c28553ba5dde2823
Gordon Beeming <me@gordonbeeming.com>
2025-10-28 04:37:22 +0000

    fix: Add rehype-slug and rehype-autolink-headings to MDXRemote
    - MDXRemote was compiling MDX at runtime without the rehype plugins
    - This caused heading IDs and anchor links to not be generated
    - Added rehypeSlug and rehypeAutolinkHeadings plugins to MDXRemote options
    - Now headings will have proper IDs like #2-auto-updating-scripts that match anchor links
    
    Co-authored-by: Gordon Beeming <me@gordonbeeming.com>

--- src/app/blog/[...slug]/page.tsx
+++ src/app/blog/[...slug]/page.tsx
@@ -14,6 +14,22 @@ import siteMetadata from '@/data/siteMetadata'
 import { notFound } from 'next/navigation'
 import { genPageMetadata } from '@/app/seo'
 import { filterPublishedPosts } from '@/utils/contentUtils'
+import rehypeSlug from 'rehype-slug'
+import rehypeAutolinkHeadings from 'rehype-autolink-headings'
+import { fromHtmlIsomorphic } from 'hast-util-from-html-isomorphic'
+
+// Icon for heading anchor links
+const icon = fromHtmlIsomorphic(
+  `
+  <span class="content-header-link">
+  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="w-5 h-5 linkicon">
+  <path d="M12.232 4.232a2.5 2.5 0 0 1 3.536 3.536l-1.225 1.224a.75.75 0 0 0 1.061 1.06l1.224-1.224a4 4 0 0 0-5.656-5.656l-3 3a4 4 0 0 0 .225 5.865.75.75 0 0 0 .977-1.138 2.5 2.5 0 0 1-.142-3.667l3-3Z" />
+  <path d="M11.603 7.963a.75.75 0 0 0-.977 1.138 2.5 2.5 0 0 1 .142 3.667l-3 3a2.5 2.5 0 0 1-3.536-3.536l1.225-1.224a.75.75 0 0 0-1.061-1.06l-1.224 1.224a4 4 0 1 0 5.656 5.656l3-3a4 4 0 0 0-.225-5.865Z" />
+  </svg>
+  </span>
+`,
+  { fragment: true }
+)
 
 const defaultLayout = 'PostLayout'
 const layouts = {
@@ -140,7 +156,27 @@ export default async function Page({ params }: { params: Promise<{ slug: string[
         dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonLd) }}
       />
       <Layout content={mainContent} authorDetails={authorDetails} next={next} prev={prev}>
-        <MDXRemote source={post.body.raw} components={components} />
+        <MDXRemote 
+          source={post.body.raw} 
+          components={components}
+          options={{
+            mdxOptions: {
+              rehypePlugins: [
+                rehypeSlug,
+                [
+                  rehypeAutolinkHeadings,
+                  {
+                    behavior: 'prepend',
+                    headingProperties: {
+                      className: ['content-header'],
+                    },
+                    content: icon,
+                  },
+                ],
+              ],
+            },
+          }}
+        />
       </Layout>
     </>
   )
